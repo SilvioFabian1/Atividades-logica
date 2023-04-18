c0f66753654b24bd37ec7d230be6445b8ee1d751
Silvio Fabian <silviofabian334@gmail.com>
2023-04-17 23:23:04 -0300

    c1 - Primeira atividade

--- ProgramaHelloWorld.java
+++ ProgramaHelloWorld.java
@@ -1,5 +1,20 @@
-class ProgramaHelloWorld {
+import java.util.Scanner;
+
+public class ProgramaHelloWorld {
+
   public static void main(String[] args) {
-    System.out.println("Olá Mundo");
+    Scanner ler = new Scanner(System.in);
+
+    System.out.println("Informe a idade: ");
+    int idade = ler.nextInt();
+
+    System.out.println("Informe seu primeiro nome: ");
+    String nome = ler.next();
+
+    System.out.println("Informe seu último nome: ");
+    String segundoNome = ler.next();
+
+    System.out.println("Olá " + nome + " " + segundoNome + " sua idade é " + idade);
+
   }
 }
