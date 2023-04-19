dd9773acd4c2a5187e4111e674a0d61b3a112988
Silvio Fabian <silviofabian334@gmail.com>
2023-04-18 22:59:58 -0300

    c12 - Atividade 7

---
+++ ProgramaMedia.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class ProgramaMedia {
+  public static void main(String[] args) {
+
+    Scanner ler = new Scanner(System.in);
+
+    System.out.println("Escreva a Primeira nota: ");
+    float nota1 = ler.nextFloat();
+
+    System.out.println("Escreva a Segunda nota: ");
+    float nota2 = ler.nextFloat();
+
+    System.out.println("Escreva a Terceira nota: ");
+    float nota3 = ler.nextFloat();
+
+    float notaFinal = (nota1 + (nota2 * 2) + (nota3 * 3)) / 6;
+
+    if (notaFinal < 60) {
+      System.out.println("Reprovado");
+    } else if (notaFinal >= 60 && notaFinal <= 100) {
+      System.out.println("Aprovado");
+    } else {
+      System.out.println("Põe as nota certa");
+    }
+
+  }
+}
