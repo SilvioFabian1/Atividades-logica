e04cd6407af4234cbb8ddb655273e653aee072aa
Thomás Tabosa <thomastabosa10@gmail.com>
2023-04-25 20:28:51 -0300

    correcoes_2

--- SomaImpares.java
+++ SomaImpares.java
@@ -1,7 +1,6 @@
 public class SomaImpares {
   public static void main(String[] args) {
 
-    // era pra somar os valores e n apenas printer eles
     for (int i = 100; i < 200; i++) {
       if (i % 2 != 0) {
         System.out.println(i);
